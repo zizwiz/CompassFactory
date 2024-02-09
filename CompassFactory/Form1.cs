fb5cfbed843297489db511f5431f8d296ffe1b0d
ziz wiz <zizwiz@engineer.com>
2024-02-09 16:43:22 +0000

    Added Nuget package

--- CompassFactory/Form1.cs
+++ CompassFactory/Form1.cs
@@ -1,13 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+/*
+< WinForm implementation of CoordinateSharp to show magnetic declination, 
+intensity, directional component, even uncertainly and much more>
+Copyright (C) < 2024 >  < ZizWiz>
+
+This program is free software: you can redistribute it and/or modify 
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 namespace CompassFactory
 {
